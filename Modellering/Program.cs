c62bbebc126fa29b8bba91743a2abc4b0900f43f
ali aldalati <ali.aldalati@elev.ga.ntig.se>
2024-09-05 11:05:21 +0200

    ilubliub

---
+++ Modellering/Program.cs
@@ -0,0 +1,11 @@
+﻿Megaman man = new Megaman() {
+Name = "Rockman",
+Hitpoints = 100
+};
+Megaman zero = new Megaman();
+
+zero.Name = "Z4ero";
+
+Console.WriteLine (man.Name);
+
+Console.ReadLine();
